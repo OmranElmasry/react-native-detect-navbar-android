ee766ad132970da55edf7e727a72ab05c0faf5d3
OmranElmasry <OmranElmasry@gmail.com>
2019-06-27 15:47:08 +0200

    update android support library to androidx

--- android/src/main/java/com/rndetectnavbarandroid/RNDetectNavbarAndroidModule.java
+++ android/src/main/java/com/rndetectnavbarandroid/RNDetectNavbarAndroidModule.java
@@ -1,30 +1,17 @@
 package com.rndetectnavbarandroid;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MenuItem;
 import android.annotation.SuppressLint;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.KeyCharacterMap;
-import android.util.DisplayMetrics;
-import android.view.KeyEvent;
-import android.view.ViewConfiguration;
-import android.view.Display;
 import android.content.Context;
-import android.util.Log;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.UiThreadUtil;
 
-/**
- * {@link NativeModule} that allows changing the appearance of the menu bar.
- */
 public class RNDetectNavbarAndroidModule extends ReactContextBaseJavaModule { 
 
   ReactApplicationContext reactContext;
